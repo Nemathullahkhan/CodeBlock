0930c110e86438be9b067602cb8a050a05546cf5
Muhammed Nemathullah Khan <115334110+Nemathullahkhan@users.noreply.github.com>
2025-02-19 01:08:34 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Appbar from "./components/Appbar";
+import { sendMail } from "@/lib/mail";
 
-export default function Home() {
+export default async function Home() {
+
+  await sendMail({to:"khancodes0@gmail.com",subject:"test",body:"Hello world"});
   return (
     <>
     {/* <Appbar/> */}
