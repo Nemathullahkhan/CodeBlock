19220da6b08ab38f487f310c8dd826218ac84d05
Muhammed Nemathullah Khan <115334110+Nemathullahkhan@users.noreply.github.com>
2025-04-12 01:29:24 +0530

    Deployment -

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig 
